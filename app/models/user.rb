00a49c4ac707746c8f5bbde0d5d79252b9a112c0
Francesca <francesca@francesca-VirtualBox.(none)>
2013-03-11 14:46:58 +0530

    Finished Sign Up

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #
 
 class User < ActiveRecord::Base
+
   attr_accessible :email, :name ,:password,:password_confirmation
   has_many :microposts
   validates :name ,presence:true, length: {maximum:50}
